a9007680af28c5c810edbda7fbcfdb4a1f15eb44
fxHHH <624238563@qq.com>
2017-10-21 20:39:15 +0800

    HHH我

---
+++ XiangMuMing/XiangMuMing/Controllers/HuQuShuiGeJiaoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XiangMuMing.Controllers
+{
+    public class HuQuShuiGeJiaoController : Controller
+    {
+        // GET: HuQuShuiGeJiao
+        public ActionResult Index()
+        {
+
+
+            int m_JiaGe;        //价格
+            int m_YouFei;
+
+            int m_YouHuiQuan=0;      //优惠券
+            int m_BaoYou=0;     //满多少包邮
+            int m_XiaDanLiJian=0;     //下单立减
+            int m_YuFuDingJin=0;       //抵用
+
+
+            int ZuiHouJiaGe;
+
+
+            //用户输入，对这些变量赋值
+
+            
+            
+
+
+
+            return View();
+        }
+
+        
+
+        
+    }
+}
---
+++ XiangMuMing/XiangMuMing/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XiangMuMing.Startup))]
+namespace XiangMuMing
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
